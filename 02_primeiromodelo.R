847548653c9a9f76f887a552f717b8a24cd899a6
CHAnnelch <carlosharaujo0811@gmail.com>
2025-04-18 19:57:01 -0300

    Primeiro Commit

---
+++ 02_primeiromodelo.R
@@ -0,0 +1,36 @@
+library(stats)
+library(lmtest)
+library(corrplot)
+library(car)
+library(dplyr)
+df <- read.csv("04_df_compilado.csv")
+
+modelo <- lm(CPIscore ~ scofree + stability + propr + govint + taxb + govspend + g_GDP + busfree + labfree + monfree + tradfree + investfree + finfree, data=df)
+summary(modelo)
+
+#R2 ajustado de 0.9291
+
+confint(modelo, level=0.95)
+
+#plot residuos
+plot(modelo$fitted.values, modelo$residuals,
+     xlab='Fitted', ylab='Residuais',
+     main='Plot dos Residuos')
+abline(0, 0)
+
+#matriz correlacao
+df_selected <- df %>% select(CPIscore, scofree, stability, propr, govint, taxb, govspend, g_GDP, busfree, labfree, monfree, tradfree, investfree, finfree, CPIrank)
+M = cor(df_selected)
+corrplot(M, method='number', tl.cex = 0.8, number.cex = 0.8)
+
+#testes
+dwtest(modelo) #a autocorrelação existe RLS.4 é
+
+bptest(modelo) #p-valor 0.1001 - n rejeita hipotese nula, n há evidencias de heteroscedasticidade
+
+vif(modelo) #scofree apresenta um valor alto de multicoli, mas isso faz sentido já que essa variável é calculada com base nas demais do Economic Freedom Index
+
+#qqnorm e qqline
+qqnorm(modelo$residuals)
+qqline(modelo$residuals)
+#distribuição aparente normal, ou possível de se aproximar à normal
