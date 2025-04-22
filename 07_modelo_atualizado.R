deca7ae0a717cc52133906ab23e63e29153aeac5
CHAnnelch <carlosharaujo0811@gmail.com>
2025-04-22 16:28:29 -0300

    Modelo atualizado com modelo alternativo de log

--- 07_modelo_atualizado.R
+++ 07_modelo_atualizado.R
@@ -9,7 +9,20 @@ library(performance)
 
 df <- read.csv("06_df_compilado_guiana.csv")
 
-modelo <- lm(CPIscore ~ stability + propr + govint + taxb + govspend + g_GDP + busfree + labfree + monfree + tradfree + investfree + finfree, data=df)
+modelo <- lm(CPIscore ~ 
+               stability +
+               propr +
+               govint +
+               taxb +
+               govspend +
+               g_GDP +
+               busfree +
+               labfree +
+               monfree +
+               tradfree +
+               investfree +
+               finfree,
+             data=df)
 summary(modelo)
 
 model_performance(modelo)
@@ -19,6 +32,7 @@ check_model(modelo)
 dev.off()
 
 check_collinearity(modelo)
+check_heteroscedasticity(modelo)
 
 #plot residuos
 plot(modelo$fitted.values, modelo$residuals,
@@ -43,6 +57,50 @@ qqnorm(modelo$residuals)
 qqline(modelo$residuals)
 #distribuição aparente normal, ou possível de se aproximar à normal
 
-#espaço para citações
-citation("performance")
-citation("corrplot")
+#transformação pra log
+CPIscore_log <- log(df$CPIscore)
+stability_log <- log(df$stability)
+propr_log <- log(df$propr)
+govint_log <- log(df$govint)
+taxb_log <- log(df$taxb)
+govspend_log <- log(df$govspend)
+g_GDP_log <- log(df$g_GDP)
+busfree_log <- log(df$busfree)
+labfree_log <- log(df$labfree)
+monfree_log <- log(df$monfree)
+tradfree_log <- log(df$tradfree)
+investfree_log <- log(df$investfree)
+finfree_log <- log(df$finfree)
+
+#aplicação do modelo log
+modelo_log <- lm(CPIscore_log ~ 
+               stability_log +
+               propr_log +
+               govint_log +
+               taxb_log +
+               govspend_log +
+               g_GDP_log +
+               busfree_log +
+               labfree_log +
+               monfree_log +
+               tradfree_log +
+               investfree_log +
+               finfree_log)
+
+summary(modelo_log)
+
+#avaliação da performance
+model_performance(modelo_log)
+
+png("check_model_log_output.png", width = 1920, height = 1080)
+check_model(modelo_log)
+dev.off()
+
+dwtest(modelo_log) #p-valor = 0.07117, RLS.4 potencialmente atendida
+
+check_collinearity(modelo_log) #multicoli alta
+
+check_heteroscedasticity(modelo_log) #heteroscedasticidade detectada
+
+#modelo log performou pior do que o modelo convencional
+#possiveis saídas: regressão robusta/bootstrap/lasso sla
