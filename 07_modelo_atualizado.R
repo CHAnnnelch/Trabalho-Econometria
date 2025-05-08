f81ffa3bf78095fde289de63729fd3a41d7053b7
CHAnnelch <carlosharaujo0811@gmail.com>
2025-05-08 14:26:37 -0300

    + modelos e redução do código que não funcionava

--- 07_modelo_atualizado.R
+++ 07_modelo_atualizado.R
@@ -11,12 +11,12 @@ library(see)
 library(plm)
 library(stargazer)
 
-
 df <- read.csv("06_df_compilado_guiana.csv")
 
 df <- df %>%
   mutate(ID_pais = group_indices(., Country))
 
+#modelo completo
 modelo <- plm(CPIscore ~ 
                stability +
                propr +
@@ -35,13 +35,63 @@ modelo <- plm(CPIscore ~
              index=c("ID_pais","Index.Year"))
 summary(modelo)
 
-#model = 'within' - modelo de efeito fixo
-
-
 model_performance(modelo)
 
-
 check_model(modelo)
+
+#modelo reduzido com variávies de gasto e liberadade
+modelo_red <- plm(CPIscore ~ 
+                taxb +
+                govspend +
+                g_GDP +
+                busfree +
+                labfree +
+                monfree +
+                tradfree +
+                investfree +
+                finfree,
+              data=df,
+              model='within',
+              index=c("ID_pais","Index.Year"))
+summary(modelo_red)
+
+model_performance(modelo_red)
+
+check_model(modelo_red)
+
+#modelo com variáveis de gastos do governo
+modelo_gastos <- plm(CPIscore ~ 
+                       taxb +
+                       govspend +
+                       g_GDP,
+                     data=df,
+                     model='within',
+                     index=c("ID_pais","Index.Year"))
+summary(modelo_gastos)
+
+model_performance(modelo_gastos)
+
+check_model(modelo_gastos)
+
+
+#modelo reduzido com variávies de liberdade
+modelo_free <- plm(CPIscore ~ 
+                    busfree +
+                    labfree +
+                    monfree +
+                    tradfree +
+                    investfree +
+                    finfree,
+                  data=df,
+                  model='within',
+                  index=c("ID_pais","Index.Year"))
+summary(modelo_free)
+
+model_performance(modelo_free)
+
+check_model(modelo_free)
+
+#model = 'within' - modelo de efeito fixo
 
 fig_performance = plot(check_model(modelo))
 ggsave(filename = "plot_performance.jpg",
@@ -62,35 +112,6 @@ M = cor(df_selected)
 
 corrplot(M, method='number', tl.cex = 0.8, number.cex = 0.8)
 
-#testes
-dwtest(modelo) #a autocorrelação existe RLS.4 é
-
-# visualizar predicoes
-library(ggeffects)
-ggeffect(modelo)
-
-plot_pred <- ggeffect(modelo) %>%
-  plot() %>%
-  sjPlot::plot_grid()
-
-plot_pred
-
-# tabelas
-library(gtsummary)
-tabela_pred <- tbl_regression(
-  modelo,
-  add_pairwise_contrasts = T,
-  pvalue_fun = ~style_pvalue(.x, digits = 3)) %>%
-  bold_p()
-
-tabela_pred
-
-#salvar tabela
-library(flextable)
-tabela_pred %>% 
-  as_flex_table() %>%
-  save_as_image(path =  "tabela_pred.png")
-
 #equacao da regressao
 library(equatiomatic)
 extract_eq(modelo)
@@ -102,121 +123,7 @@ library(effectsize)
 
 tab_model(modelo, show.intercept = F)
 
-ef_size <- eta_squared(modelo) %>%
-  mutate(Interpret = interpret_eta_squared(Eta2_partial))
-
-ef_size
-
-ef_size %>%
-  ggplot(aes(x= reorder(Parameter, Eta2_partial),
-             y= Eta2_partial))+
-  geom_bar(stat="identity")+
-  geom_text(aes(label=Interpret),
-            size = 4, hjust = -0.1, fontface = "bold")+
-  coord_flip()+
-  xlab("")+
-  ylim(0, .99)
-
-performance(modelo)
-
 interpret_r2(0.262, rules = "hair2011")
-
-?interpret_r2
-
-#transformação pra log
-CPIscore_log <- log(df$CPIscore)
-stability_log <- log(df$stability)
-propr_log <- log(df$propr)
-govint_log <- log(df$govint)
-taxb_log <- log(df$taxb)
-govspend_log <- log(df$govspend)
-g_GDP_log <- log(df$g_GDP)
-busfree_log <- log(df$busfree)
-labfree_log <- log(df$labfree)
-monfree_log <- log(df$monfree)
-tradfree_log <- log(df$tradfree)
-investfree_log <- log(df$investfree)
-finfree_log <- log(df$finfree)
-
-#aplicação do modelo log
-modelo_log <- lm(CPIscore_log ~ 
-               stability_log +
-               propr_log +
-               govint_log +
-               taxb_log +
-               govspend_log +
-               g_GDP_log +
-               busfree_log +
-               labfree_log +
-               monfree_log +
-               tradfree_log +
-               investfree_log +
-               finfree_log)
-
-summary(modelo_log)
-
-check_model(modelo_log)
-
-check_normality(modelo_log)
-check_collinearity(modelo_log)
-check_heteroscedasticity(modelo_log)
-check_outliers(modelo_log)
-
-dwtest(modelo_log) #p-valor = 0.07117, RLS.4 potencialmente atendida
-
-# visualizar predicoes
-library(ggeffects)
-ggeffect(modelo_log)
-
-plot_pred <- ggeffect(modelo_log) %>%
-  plot() %>%
-  sjPlot::plot_grid()
-
-plot_pred
-
-# tabelas
-library(gtsummary)
-tabela_pred <- tbl_regression(
-  modelo_log,
-  add_pairwise_contrasts = T,
-  pvalue_fun = ~style_pvalue(.x, digits = 3)) %>%
-  bold_p()
-
-#salvar tabela
-library(flextable)
-tabela_pred %>% 
-  as_flex_table() %>%
-  save_as_image(path =  "tabela_pred_log.png")
-
-#equacao da regressao
-library(equatiomatic)
-extract_eq(modelo_log)
-
-#mais coisas
-library(emmeans)
-library(sjPlot)
-library(effectsize)
-
-tab_model(modelo_log, show.intercept = F)
-
-ef_size <- eta_squared(modelo_log) %>%
-  mutate(Interpret = interpret_eta_squared(Eta2_partial))
-
-ef_size
-
-ef_size %>%
-  ggplot(aes(x= reorder(Parameter, Eta2_partial),
-             y= Eta2_partial))+
-  geom_bar(stat="identity")+
-  geom_text(aes(label=Interpret),
-            size = 4, hjust = -0.1, fontface = "bold")+
-  coord_flip()+
-  xlab("")+
-  ylim(0, .99)
-
-performance(modelo_log)
-
-interpret_r2(0.944, rules = "hair2011")
 
 ?interpret_r2
 
